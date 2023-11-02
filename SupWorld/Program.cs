659eacece9acf488a12d67371e6d59062f77ba05
Micah Ammons <micahammons@gmail.com>
2023-11-02 11:11:58 -0400

    practice updating it on Github

--- SupWorld/Program.cs
+++ SupWorld/Program.cs
@@ -1,2 +1,2 @@
-﻿// Read this and you'll be cursed for 3 minutes
+﻿// Read this and you'll be cursed for 3 minutes unless you send this to 38 of your close friends 
 Console.WriteLine("Sup, World!");
